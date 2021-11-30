d4e5b34685e8edb632164e0fa0af98f33dd1e8af
Kevin Cotugno <kevin@pulchre.net>
2021-11-30 06:57:24 -0700

    Add wait/cancel to processor pool

--- processor_pool.go
+++ processor_pool.go
@@ -1,9 +1,15 @@
 package wingman
 
 type ProcessorPool interface {
-	// Get locks and returns a processor from the pool
+	// Get locks and returns a processor from the pool.
 	Get() Processor
 
-	// Put releases the processor with the given back to the pool
+	// Put releases the processor with the given back to the pool.
 	Put(string)
+
+	// Wait blocks until a processor is available in the pool.
+	Wait()
+
+	// Cancel unblocks a call to wait.
+	Cancel()
 }
