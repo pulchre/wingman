fc052804a90a8507e19bdf8150f67754d8c57dd6
Kevin Cotugno <kevin@pulchre.net>
2022-01-03 15:22:24 -0700

    Move log test helpers to mock package

--- mock/log.go
+++ mock/log.go
@@ -1,22 +1,35 @@
-package wingman
+package mock
+
+import (
+	"sync"
+
+	"github.com/pulchre/wingman"
+)
 
 var TestLog *TestLogger
 
 func init() {
 	TestLog = &TestLogger{}
-	Log = TestLog
+	wingman.Log = TestLog
 }
 
 type TestLogger struct {
 	FatalVars [][]interface{}
 	PrintVars [][]interface{}
+	mu        sync.Mutex
 }
 
 func (l *TestLogger) Fatal(v ...interface{}) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
 	l.FatalVars = append(l.FatalVars, v)
 }
 
 func (l *TestLogger) Fatalf(format string, v ...interface{}) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
 	if l.FatalVars == nil {
 		l.FatalVars = make([][]interface{}, 0)
 	}
@@ -25,10 +38,16 @@ func (l *TestLogger) Fatalf(format string, v ...interface{}) {
 }
 
 func (l *TestLogger) Print(v ...interface{}) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
 	l.PrintVars = append(l.PrintVars, v)
 }
 
 func (l *TestLogger) Printf(format string, v ...interface{}) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
 	if l.PrintVars == nil {
 		l.FatalVars = make([][]interface{}, 0)
 	}
@@ -36,7 +55,10 @@ func (l *TestLogger) Printf(format string, v ...interface{}) {
 	l.PrintVars = append(l.PrintVars, append([]interface{}{format}, v...))
 }
 
-func (l TestLogger) FatalReceived(v ...interface{}) bool {
+func (l *TestLogger) FatalReceived(v ...interface{}) bool {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
 	for _, msg := range l.FatalVars {
 		if len(msg) != len(v) {
 			continue
@@ -56,10 +78,17 @@ func (l TestLogger) FatalReceived(v ...interface{}) bool {
 	return false
 }
 
-func (l TestLogger) PrintReceived(v ...interface{}) bool {
+func (l *TestLogger) PrintReceived(v ...interface{}) bool {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
 	for _, msg := range l.PrintVars {
-		if len(msg) != len(v) {
+		if len(msg) != len(v) && len(v) != 1 {
 			continue
+		}
+
+		if len(v) == 1 && msg[0] == v[0] {
+			return true
 		}
 
 		for j, val := range v {
@@ -77,6 +106,9 @@ func (l TestLogger) PrintReceived(v ...interface{}) bool {
 }
 
 func (l *TestLogger) Reset() {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
 	l.FatalVars = nil
 	l.PrintVars = nil
 }
