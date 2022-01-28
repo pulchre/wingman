007b595789c91ebda1bc8dbf3ab772680f6bdb48
Kevin Cotugno <kevin@pulchre.net>
2022-01-27 20:44:08 -0700

    Add goroutine Processor.Stop test

--- processor/goroutine/processor_test.go
+++ processor/goroutine/processor_test.go
@@ -3,6 +3,7 @@ package goroutine
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/pulchre/wingman/mock"
 )
@@ -18,5 +19,21 @@ func TestStart(t *testing.T) {
 
 	if !mock.TestLog.PrintReceived(fmt.Sprintf("Processor id=%v status=idle", p.Id())) {
 		t.Errorf("Expected to receive `idle` status for processor")
+	}
+}
+
+func TestStop(t *testing.T) {
+	mock.TestLog.Reset()
+	p := NewProcessor()
+
+	p.Start()
+	p.Stop()
+	if !mock.TestLog.PrintReceived(fmt.Sprintf("Processor id=%v status=stopping", p.Id())) {
+		t.Errorf("Expected to receive `stopping` status for processor")
+	}
+
+	time.Sleep(10 * time.Millisecond)
+	if !mock.TestLog.PrintReceived(fmt.Sprintf("Processor id=%v status=dead", p.Id())) {
+		t.Errorf("Expected to receive `dead` status for processor")
 	}
 }
