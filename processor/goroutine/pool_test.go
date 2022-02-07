f6ea05e9b4b38e2d38d0ad53ded1f258224a6792
Kevin Cotugno <kevin@pulchre.net>
2022-02-06 21:20:23 -0700

    Add tests for goroutine.Pool

---
+++ processor/goroutine/pool_test.go
@@ -0,0 +1,91 @@
+package goroutine
+
+import (
+	"testing"
+	"time"
+)
+
+func TestNewPool(t *testing.T) {
+	options := PoolOptions{Concurrency: 0}
+
+	p, err := NewPool(options)
+	if err != nil {
+		t.Error("Expected nil error. Got: ", err)
+
+	}
+
+	pool := p.(*Pool)
+	if pool.options.Concurrency != 1 {
+		t.Error("Expected Concurrency to be set to 1, got: ", pool.options.Concurrency)
+	}
+}
+
+func TestPoolGet(t *testing.T) {
+	p, _ := NewPool(PoolOptions{})
+
+	proc := p.Get()
+
+	if proc == nil {
+		t.Error("Expected returned processor. Got nil")
+	}
+
+	go func() { p.Cancel() }()
+
+	proc2 := p.Get()
+	if proc2 != nil {
+		t.Error("Expected nil returned processor")
+	}
+}
+
+func TestPoolPut(t *testing.T) {
+	p, _ := NewPool(PoolOptions{})
+
+	proc := p.Get()
+	if proc == nil {
+		t.Error("Expected returned processor. Got nil")
+	}
+
+	p.Put(proc.Id())
+
+	proc2 := p.Get()
+	if proc.Id() != proc2.Id() {
+		t.Errorf("Expected processor to be reused with id: %s. Got: %s", proc.Id(), proc2.Id())
+	}
+
+	// Should not panic!
+	p.Put("invalid")
+}
+
+// This tests that the close method doesn't hang.
+func TestPoolClose(t *testing.T) {
+	p, _ := NewPool(PoolOptions{Concurrency: 5})
+
+	proc1 := p.Get()
+	proc2 := p.Get()
+	proc3 := p.Get()
+	proc4 := p.Get()
+
+	go func() {
+		p.Put(proc1.Id())
+		p.Put(proc2.Id())
+		p.Put(proc3.Id())
+		p.Put(proc4.Id())
+	}()
+
+	p.Close()
+}
+
+// This tests that the close method doesn't hang.
+func TestPoolForceClose(t *testing.T) {
+	p, _ := NewPool(PoolOptions{Concurrency: 5})
+
+	p.Get()
+	p.Get()
+	p.Get()
+	p.Get()
+
+	go func() { p.Close() }()
+	time.Sleep(25 * time.Millisecond)
+
+	p.ForceClose()
+}
