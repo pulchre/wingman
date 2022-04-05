3483870687b5665ab3fb46fddcd12662842ec0af
Kevin Cotugno <kevin@pulchre.net>
2022-04-05 07:17:02 -0700

    Add native handler tests

--- processor/native/handle_test.go
+++ processor/native/handle_test.go
@@ -1,1 +1,49 @@
 package native
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/pulchre/wingman/mock"
+)
+
+func TestHandle(t *testing.T) {
+	pool, err := NewPool(DefaultOptions())
+	if err != nil {
+		panic(err)
+	}
+	defer pool.ForceClose()
+
+	p := pool.Get()
+	if p == nil {
+		t.Fatalf("Expected processor got nil")
+	}
+	defer pool.Put(p.Id())
+
+	if p.Results() != nil {
+		t.Error("Expected nil Results chan when no job is sent")
+	}
+
+	if p.Done() != nil {
+		t.Error("Expected nil Done chan when no job is sent")
+	}
+
+	j := mock.NewWrappedJob()
+	p.SendJob(j)
+
+	if p.Results() == nil {
+		t.Error("Expected non-nil Results chan when job is sent")
+	}
+
+	if p.Done() == nil {
+		t.Error("Expected non-nil Done chan when job is sent")
+	}
+
+	results := <-p.Results()
+
+	if results.Job.ID == uuid.Nil {
+		t.Error("Expected results got nil")
+	}
+
+	<-p.Done()
+}
