646169321eced7ed5ecb4aa42c2963faec434a00
Kevin Cotugno <kevin@pulchre.net>
2022-05-17 12:08:28 -0700

    Add ContextKey for storing processor ID in the Context

--- processor.go
+++ processor.go
@@ -18,6 +18,14 @@ var ProcessorStatuses = map[ProcessorStatus]string{
 
 type ProcessorStatus int
 
+// ContextKey defines the type for our context value keys. We need this per the
+// documentation for Context that states that the keys should not be a standard
+// type.
+type ContextKey string
+
+// ContextProcessorIDKey is the key under which the processor ID is stored.
+const ContextProcessorIDKey = ContextKey("processorID")
+
 type Processor interface {
 	Id() string
 	SendJob(InternalJob) error
